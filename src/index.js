12e569a3fb1924e65acde588bc8e8a4675a9d0d3
Henrique Leite <henriqueleite616@gmail.com>
2021-09-19 20:05:00 -0300

    Fix Routing

--- src/index.js
+++ src/index.js
@@ -11,11 +11,19 @@ import './assets/scss/style.scss';
 
 const history = createBrowserHistory();
 
+// 1. Set up the browser history with the updated location
+// (minus the # sign)
+const path = (/#!(\/.*)$/.exec(document.location.hash) || [])[1];
+
+if (path) {
+	history.replace(path);
+}
+
 ReactDOM.render(
-  <Router history={history}>
-    <App />
-  </Router>,
-  document.getElementById('root')
+	<Router history={history}>
+		<App />
+	</Router>,
+	document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
